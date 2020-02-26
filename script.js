a029c88851286466bd8e55aa6a3896b3fd346357
Julio <j.a.pagan.q@gmail.com>
2020-02-25 22:07:59 -0600

    complete js validation

--- script.js
+++ script.js
@@ -3,5 +3,22 @@ function Response(fname, lname, email, pnumber, dob) {
         alert("Enter first name");
         return false;
     }
+    if(lname.value == ""){
+        alert("Enter last name");
+        return false;
+    }
+    if(email.value == ""){
+        alert("Enter email address");
+        return false;
+    }
+    if(pnumber.value == ""){
+        alert("Enter phone number");
+        return false;
+    }
+    if(dob.value == ""){
+        alert("Enter date of birth");
+        return false;
+    }
+    alert("Sign up successful");
     return false;
   }
