96449a8c30ed0add67881e980fc96d23faf60c73
Julio <j.a.pagan.q@gmail.com>
2020-02-25 21:50:53 -0600

    css done now editing javascript

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function Response() {
+    var fn = document.forms["signup"]["fname"];
+    if(fn == ""){
+        alert("Enter first name");
+        return false;
+    }
     alert("Sign up successful");
     return false;
   }
