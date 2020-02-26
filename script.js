f2086c5a6c702726f3bcb399061cde3b56ef9d8c
Julio <j.a.pagan.q@gmail.com>
2020-02-25 22:48:19 -0600

    testing

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function Response(fname, lname, email, pnumber, dob) {
     if(fname.value == ""){
         alert("Enter first name");
         return false;
-    }else if(fname.value == {
+    }else if(fname.value.has{
 
     }
     if(lname.value == ""){
@@ -21,6 +21,7 @@ function Response(fname, lname, email, pnumber, dob) {
         alert("Enter date of birth");
         return false;
     }
+    
     alert("Sign up successful");
     return false;
   }
